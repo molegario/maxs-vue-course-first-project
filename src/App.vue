f4654474e9563c0042b3f016353cff06c3b36808
molegario <molegario@x2omedia.com>
2025-02-23 09:40:12 -0500

    adapted friends list example

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,75 @@
 <template>
-  <div>
-    <h1>{{ appMsg }}</h1>
+  <header>
+    <h1>Friends List</h1>
+  </header>
+  <section>
     <ul>
-      <HelloWorld v-for="message in appMsgArr" :msg="message.msg" :key="message.id"/>
+      <friend-contact v-for="friend in friends" :key="friend.id" :friend="friend" />
     </ul>
-  </div>  
+  </section>
 </template>
 
 <script>
-  import HelloWorld from './components/HelloWorld.vue'
+import FriendContact from "./components/FriendContact.vue";
 
-  export default {
-    name: 'App',
-    components: {
-      HelloWorld
-    },
-    data() {
-      return {
-        appMsg: 'Welcome to Your Vue.js App XXX',
-        appMsgArr: [
-          {
-            id: "v001",
-            msg: 'Welcome to Your Vue.js App 1',
-          },
-          {
-            id: "v002",
-            msg: 'Welcome to Your Vue.js App 2',
-          },
-          {
-            id: "v003",
-            msg: 'Welcome to Your Vue.js App 3',
-          },
-          {
-            id: "v004",
-            msg: 'Welcome to Your Vue.js App 4',
-          },
-          {
-            id: "v005",
-            msg: 'Welcome to Your Vue.js App 5',
-          },
-        ],
-      };
-    },
-  }
-
+export default {
+  name: "App",
+  components: {
+    FriendContact,
+  },
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "01234 5678 991",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "09876 543 221",
+          email: "julie@localhost.com",
+        },
+      ],
+    };
+  },
+};
 </script>
-
 
 <style>
 
+@import url('https://fonts.googleapis.com/css2?family=Jost&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Jost", sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #58004d;
+  color: white;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
 
 </style>
