41e1c6edc0145ec91617efc94f2085c3fd3d251e
molegario <molegario@x2omedia.com>
2025-02-23 08:33:05 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <h1>{{ appMsg }}</h1>
+    <ul>
+      <HelloWorld v-for="message in appMsgArr" :msg="message.msg" :key="message.id"/>
+    </ul>
+  </div>  
+</template>
+
+<script>
+  import HelloWorld from './components/HelloWorld.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      HelloWorld
+    },
+    data() {
+      return {
+        appMsg: 'Welcome to Your Vue.js App XXX',
+        appMsgArr: [
+          {
+            id: "v001",
+            msg: 'Welcome to Your Vue.js App 1',
+          },
+          {
+            id: "v002",
+            msg: 'Welcome to Your Vue.js App 2',
+          },
+          {
+            id: "v003",
+            msg: 'Welcome to Your Vue.js App 3',
+          },
+          {
+            id: "v004",
+            msg: 'Welcome to Your Vue.js App 4',
+          },
+          {
+            id: "v005",
+            msg: 'Welcome to Your Vue.js App 5',
+          },
+        ],
+      };
+    },
+  }
+
+</script>
+
+
+<style>
+
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <li>{{ internalMsg }} - {{ msg }}</li>
+</template>
+<script>
+  export default {
+    name: 'HelloWorld',
+    props: {
+      msg: String
+    },
+    data () {
+      return {
+        internalMsg: 'Hello World'
+      };
+    }
+  }
+</script>
+<style></style>
